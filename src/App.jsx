ce2b304238890647b67ab1804277be1e63e0d5fe
Max Qiu <maxzqiu@gmail.com>
2024-11-16 10:32:53 -0800

    added location

--- src/App.jsx
+++ src/App.jsx
@@ -215,6 +215,10 @@ function App() {
               setLINK("https://api.weather.gov/gridpoints/LOX/174,44/forecast")
               setLocation("Claremont")
             }}>Claremont</button>
+            <button onClick={()=>{
+              setLINK("https://api.weather.gov/gridpoints/SGX/89,59/forecast")
+              setLocation("Palm Springs International Airport, Palm Springs (KPSP)")
+            }}>Palm Springs Intl Apt (KPSP)</button>
           </div>
           <canvas ref={canvasRef} width="500" height="500"></canvas>
           
